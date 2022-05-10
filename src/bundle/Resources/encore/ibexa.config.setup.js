c6a743bec9c8ddae4976f6f6c9cd3623510ff1bb
Jakub Brzegowski <symfiz@gmail.com>
2022-05-10 14:04:13 +0200

    fix alias path

--- src/bundle/Resources/encore/ibexa.config.setup.js
+++ src/bundle/Resources/encore/ibexa.config.setup.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = (Encore) => {
     Encore.addAliases({
-        '@react-dom': path.resolve('../pubic/vendors/react-dom'),
+        '@react-dom': path.resolve('./public/bundles/ibexaadminuiassets/vendors/react-dom'),
     });
 };
