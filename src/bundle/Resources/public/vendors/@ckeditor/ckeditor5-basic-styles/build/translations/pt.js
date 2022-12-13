ef7967ae491862e2f31f7a1a7f6f5717ef0b0ecf
Dariusz Szut <dew326@gmail.com>
2022-12-13 12:31:08 +0100

    Version 4.2.4

---
+++ src/bundle/Resources/public/vendors/@ckeditor/ckeditor5-basic-styles/build/translations/pt.js
@@ -0,0 +1,1 @@
+!function(i){const o=i.pt=i.pt||{};o.dictionary=Object.assign(o.dictionary||{},{Bold:"Negrito",Code:"Código",Italic:"Itálico",Strikethrough:"Riscado",Subscript:"",Superscript:"",Underline:"Sublinhado"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
