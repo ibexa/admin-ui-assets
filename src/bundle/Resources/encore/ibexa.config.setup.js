a14f158bd4f5a89e4ecb1435ae247ef0d376ac31
Michał Grabowski <michal.grabowski@ibexa.co>
2022-12-20 14:45:19 +0100

    IBX-4327: Added alias for assets

--- src/bundle/Resources/encore/ibexa.config.setup.js
+++ src/bundle/Resources/encore/ibexa.config.setup.js
@@ -1,7 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = (Encore) => {
-    Encore.addAliases({
-        '@react-dom': path.resolve('./public/bundles/ibexaadminuiassets/vendors/react-dom'),
-    });
+  Encore.addAliases({
+    "@react-dom": path.resolve(
+      "./public/bundles/ibexaadminuiassets/vendors/react-dom"
+    ),
+    "@ibexa-admin-ui-assets": path.resolve("./vendor/ibexa/admin-ui-assets"),
+  });
 };
