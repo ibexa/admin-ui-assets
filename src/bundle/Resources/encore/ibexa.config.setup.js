6b67a7367adea1e90ba04a745bc97a8631b66cef
Michał Grabowski <michal.grabowski@ibexa.co>
2023-01-10 12:10:38 +0100

    code style

--- src/bundle/Resources/encore/ibexa.config.setup.js
+++ src/bundle/Resources/encore/ibexa.config.setup.js
@@ -1,10 +1,8 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = (Encore) => {
-  Encore.addAliases({
-    "@react-dom": path.resolve(
-      "./public/bundles/ibexaadminuiassets/vendors/react-dom"
-    ),
-    "@ibexa-admin-ui-assets": path.resolve("./vendor/ibexa/admin-ui-assets"),
-  });
+    Encore.addAliases({
+        '@react-dom': path.resolve('./public/bundles/ibexaadminuiassets/vendors/react-dom'),
+        '@ibexa-admin-ui-assets': path.resolve('./vendor/ibexa/admin-ui-assets'),
+    });
 };
