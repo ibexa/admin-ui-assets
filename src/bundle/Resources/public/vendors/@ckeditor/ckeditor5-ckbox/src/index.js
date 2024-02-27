3b1fc8c30f071559ea87e6953abb858e6de141e2
Dariusz Szut <dew326@gmail.com>
2024-02-27 15:25:30 +0100

    Version 4.6.1

---
+++ src/bundle/Resources/public/vendors/@ckeditor/ckeditor5-ckbox/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+/**
+ * @module ckbox
+ */
+export { default as CKBox } from './ckbox';
+export { default as CKBoxEditing } from './ckboxediting';
+export { default as CKBoxUI } from './ckboxui';
+export { default as CKBoxImageEditEditing } from './ckboximageedit/ckboximageeditediting';
+export { default as CKBoxImageEditUI } from './ckboximageedit/ckboximageeditui';
+export { default as CKBoxImageEdit } from './ckboximageedit';
+import './augmentation';
