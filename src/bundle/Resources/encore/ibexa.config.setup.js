145f29367ccb83658d44c8847c25042eb8e21ce5
Michał Grabowski <michal.grabowski@ibexa.co>
2025-05-05 12:21:11 +0200

    design system aliases

--- src/bundle/Resources/encore/ibexa.config.setup.js
+++ src/bundle/Resources/encore/ibexa.config.setup.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = (Encore) => {
     Encore.addAliases({
-        '@ibexa-admin-ui-assets': path.resolve(
-            './vendor/ibexa/admin-ui-assets'
-        ),
+        '@ibexa-admin-ui-assets': path.resolve('./vendor/ibexa/admin-ui-assets'),
+        '@ds-assets': path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/design-system-assets/dist'),
+        '@ds-components': path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/design-system-components/dist'),
     });
 };
