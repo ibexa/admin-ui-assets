585f4712a0520334f9e5aa335e6bc3a437f05d1e
Michał Grabowski <valygar92@gmail.com>
2025-05-22 11:29:31 +0200

    IBX-9939: Design System aliases (#30)

--- bin/prepare_release.sh
+++ bin/prepare_release.sh
@@ -42,39 +42,39 @@ check_process()
 }
 
 check_command "git"
-check_command "npm"
+check_command "yarn"
 
-VENDOR_DIR="src/bundle/Resources/public/vendors/"
+VENDOR_DIR="src/bundle/Resources/public/vendors"
 BOOTSTRAP_DIR="$VENDOR_DIR/bootstrap"
-BOOTSTRAP_NOTICE="$BOOTSTRAP_DIR/BOOTSTRAP_IN_EZPLATFORMADMINUIASSETS.txt"
+BOOTSTRAP_NOTICE="$BOOTSTRAP_DIR/BOOTSTRAP_IN_ADMINUIASSETS.txt"
 FLATPICKR_DIR="$VENDOR_DIR/flatpickr"
-FLATPICKR_NOTICE="$FLATPICKR_DIR/FLATPICKR_IN_EZPLATFORMADMINUIASSETS.txt"
+FLATPICKR_NOTICE="$FLATPICKR_DIR/FLATPICKR_IN_ADMINUIASSETS.txt"
 JQUERY_DIR="$VENDOR_DIR/jquery"
-JQUERY_NOTICE="$JQUERY_DIR/JQUERY_IN_EZPLATFORMADMINUIASSETS.txt"
+JQUERY_NOTICE="$JQUERY_DIR/JQUERY_IN_ADMINUIASSETS.txt"
 LEAFLET_DIR="$VENDOR_DIR/leaflet"
-LEAFLET_NOTICE="$LEAFLET_DIR/LEAFLET_IN_EZPLATFORMADMINUIASSETS.txt"
+LEAFLET_NOTICE="$LEAFLET_DIR/LEAFLET_IN_ADMINUIASSETS.txt"
 POPPER_DIR="$VENDOR_DIR/@popperjs"
-POPPER_NOTICE="$POPPER_DIR/POPPER_IN_EZPLATFORMADMINUIASSETS.txt"
+POPPER_NOTICE="$POPPER_DIR/POPPER_IN_ADMINUIASSETS.txt"
 REACT_DIR="$VENDOR_DIR/react"
-REACT_NOTICE="$REACT_DIR/REACT_IN_EZPLATFORMADMINUIASSETS.txt"
+REACT_NOTICE="$REACT_DIR/REACT_IN_ADMINUIASSETS.txt"
 REACT_DOM_DIR="$VENDOR_DIR/react-dom"
-REACT_DOM_NOTICE="$REACT_DOM_DIR/REACT_DOM_IN_EZPLATFORMADMINUIASSETS.txt"
+REACT_DOM_NOTICE="$REACT_DOM_DIR/REACT_DOM_IN_ADMINUIASSETS.txt"
 TAGGIFY_DIR="$VENDOR_DIR/taggify"
-TAGGIFY_NOTICE="$TAGGIFY_DIR/TAGGIFY_IN_EZPLATFORMADMINUIASSETS.txt"
+TAGGIFY_NOTICE="$TAGGIFY_DIR/TAGGIFY_IN_ADMINUIASSETS.txt"
 MOMENT_DIR="$VENDOR_DIR/moment"
-MOMENT_NOTICE="$MOMENT_DIR/MOMENT_IN_EZPLATFORMADMINUIASSETS.txt"
+MOMENT_NOTICE="$MOMENT_DIR/MOMENT_IN_ADMINUIASSETS.txt"
 MOMENT_TIMEZONE_DIR="$VENDOR_DIR/moment-timezone"
-MOMENT_TIMEZONE_NOTICE="$MOMENT_TIMEZONE_DIR/MOMENT_TIMEZONE_IN_EZPLATFORMADMINUIASSETS.txt"
+MOMENT_TIMEZONE_NOTICE="$MOMENT_TIMEZONE_DIR/MOMENT_TIMEZONE_IN_ADMINUIASSETS.txt"
 D3_DIR="$VENDOR_DIR/d3"
-D3_NOTICE="$D3_DIR/D3_IN_EZPLATFORMADMINUIASSETS.txt"
+D3_NOTICE="$D3_DIR/D3_IN_ADMINUIASSETS.txt"
 DAGRE_D3_DIR="$VENDOR_DIR/dagre-d3"
-DAGRE_D3_NOTICE="$DAGRE_D3_DIR/DAGRE_D3_IN_EZPLATFORMADMINUIASSETS.txt"
+DAGRE_D3_NOTICE="$DAGRE_D3_DIR/DAGRE_D3_IN_ADMINUIASSETS.txt"
 JS_MD5_DIR="$VENDOR_DIR/js-md5"
-JS_MD5_NOTICE="$JS_MD5_DIR/JS_MD5_IN_EZPLATFORMADMINUIASSETS.txt"
+JS_MD5_NOTICE="$JS_MD5_DIR/JS_MD5_IN_ADMINUIASSETS.txt"
 CHART_JS_DIR="$VENDOR_DIR/chart-js"
-CHART_JS_NOTICE="$CHART_JS_DIR/CHART_JS_IN_EZPLATFORMADMINUIASSETS.txt"
+CHART_JS_NOTICE="$CHART_JS_DIR/CHART_JS_IN_ADMINUIASSETS.txt"
 CHARTJS_PLUGIN_DATALABELS_DIR="$VENDOR_DIR/chartjs-plugin-datalabels"
-CHARTJS_PLUGIN_DATALABELS_NOTICE="$CHARTJS_PLUGIN_DATALABELS_DIR/CHARTJS_PLUGIN_DATALABELS_IN_EZPLATFORMADMINUIASSETS.txt"
+CHARTJS_PLUGIN_DATALABELS_NOTICE="$CHARTJS_PLUGIN_DATALABELS_DIR/CHARTJS_PLUGIN_DATALABELS_IN_ADMINUIASSETS.txt"
 
 CURRENT_BRANCH=`git branch | grep '*' | cut -d ' ' -f 2`
 TMP_BRANCH="version_$VERSION"
@@ -89,115 +89,93 @@ echo "# Removing the assets"
 [ -d "$VENDOR_DIR" ] && rm -rf $VENDOR_DIR/*
 check_process "clean the vendor dir $VENDOR_DIR"
 
-echo "# Removing package-lock.json"
-rm -rf "package-lock.json"
+echo "# Removing yarn.lock"
+rm "yarn.lock"
 
 echo "# Installing dependendencies"
-npm install
-npm run prepare-release
+yarn install
+yarn run prepare-release
 
 echo "# Removing unused files from Bootstrap"
-rm -rf "$BOOTSTRAP_DIR/build" "$BOOTSTRAP_DIR/dist/css" "$BOOTSTRAP_DIR/js" $BOOTSTRAP_DIR/dist/js/bootstrap.js $BOOTSTRAP_DIR/dist/js/bootstrap.js.map $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.js $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.js.map $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.min.js $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.min.js.map $BOOTSTRAP_DIR/.eslintignore $BOOTSTRAP_DIR/Gemfile
+rm -r "$BOOTSTRAP_DIR/dist/css" "$BOOTSTRAP_DIR/js" $BOOTSTRAP_DIR/dist/js/bootstrap.js $BOOTSTRAP_DIR/dist/js/bootstrap.js.map $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.js $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.js.map $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.min.js $BOOTSTRAP_DIR/dist/js/bootstrap.bundle.min.js.map
 check_process "clean bootstrap"
 echo "This is a customized Bootstrap version." > $BOOTSTRAP_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $BOOTSTRAP_NOTICE
 
 echo "# Removing unused files from Flatpickr"
-rm -rf "$FLATPICKR_DIR/src" $FLATPICKR_DIR/dist/flatpickr.css $FLATPICKR_DIR/dist/flatpickr.js $FLATPICKR_DIR/dist/ie.css $FLATPICKR_DIR/.jest.json $FLATPICKR_DIR/.jest.ssr.json $FLATPICKR_DIR/.travis.yml $FLATPICKR_DIR/index.d.ts $FLATPICKR_DIR/tsconfig.json $FLATPICKR_DIR/README.md
+rm -r "$FLATPICKR_DIR/src" $FLATPICKR_DIR/dist/flatpickr.css $FLATPICKR_DIR/dist/flatpickr.js $FLATPICKR_DIR/dist/ie.css $FLATPICKR_DIR/README.md
 check_process "clean flatpickr"
 echo "This is a customized Flatpickr version." > $FLATPICKR_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $FLATPICKR_NOTICE
 
 echo "# Removing unused files from jQuery"
-rm -rf "$JQUERY_DIR/external" "$JQUERY_DIR/src" $JQUERY_DIR/dist/core.js $JQUERY_DIR/dist/jquery.js $JQUERY_DIR/dist/jquery.min.map $JQUERY_DIR/dist/jquery.slim.js $JQUERY_DIR/dist/jquery.slim.min.js $JQUERY_DIR/dist/jquery.slim.min.map $JQUERY_DIR/AUTHORS.txt $JQUERY_DIR/bower.json $JQUERY_DIR/README.md
+rm -r "$JQUERY_DIR/src" $JQUERY_DIR/dist/jquery.js $JQUERY_DIR/dist/jquery.min.map $JQUERY_DIR/dist/jquery.slim.js $JQUERY_DIR/dist/jquery.slim.min.js $JQUERY_DIR/dist/jquery.slim.min.map $JQUERY_DIR/AUTHORS.txt $JQUERY_DIR/bower.json $JQUERY_DIR/README.md
 check_process "clean jquery"
 echo "This is a customized jQuery version." > $JQUERY_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $JQUERY_NOTICE
 
 echo "# Removing unused files from Leaflet"
-rm -rf "$LEAFLET_DIR/src" $LEAFLET_DIR/CHANGELOG.md $LEAFLET_DIR/README.md
+rm -r "$LEAFLET_DIR/src" $LEAFLET_DIR/CHANGELOG.md $LEAFLET_DIR/README.md
 check_process "clean Leaflet"
 echo "This is a customized Leaflet version." > $LEAFLET_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $LEAFLET_NOTICE
 
 echo "# Removing unused files from Popper"
-rm -rf "$POPPER_DIR/core/dist/esm" "$POPPER_DIR/core/.tmp" $POPPER_DIR/core/dist/umd/popper-utils.js $POPPER_DIR/core/dist/umd/popper-utils.js.map $POPPER_DIR/core/dist/umd/popper-utils.min.js $POPPER_DIR/core/dist/umd/popper-utils.min.js.map $POPPER_DIR/core/dist/umd/popper.js $POPPER_DIR/core/dist/umd/popper.js.map $POPPER_DIR/index.d.ts
+rm -r "$POPPER_DIR/core/dist/esm" $POPPER_DIR/core/dist/umd/popper.js $POPPER_DIR/core/dist/umd/popper.js.map
 check_process "clean popperjs"
 echo "This is a customized Popperjs version." > $POPPER_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $POPPER_NOTICE
 
-echo "# Removing unused files from react"
-rm -rf "$REACT_DIR/lib" "$REACT_DIR/node_modules" $REACT_DIR/react.js $REACT_DIR/dist/react-with-addons.js $REACT_DIR/dist/react-with-addons.min.js
-check_process "clean React"
 echo "This is a customized React version." > $REACT_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $REACT_NOTICE
 
 echo "# Removing unused files from react-dom"
-rm -rf "$REACT_DOM_DIR/lib" "$REACT_DOM_DIR/node_modules" $REACT_DOM_DIR/index.js $REACT_DOM_DIR/server.js $REACT_DOM_DIR/test-utils.js $REACT_DOM_DIR/dist/react-dom-server.js $REACT_DOM_DIR/dist/react-dom-server.min.js
+rm -r $REACT_DOM_DIR/index.js $REACT_DOM_DIR/server.js $REACT_DOM_DIR/test-utils.js
 check_process "clean ReactDOM"
 echo "This is a customized ReactDOM version." > $REACT_DOM_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $REACT_DOM_NOTICE
 
 echo "# Removing unused files from taggify"
-rm -rf "$TAGGIFY_DIR/test" "$TAGGIFY_DIR/src/css" $TAGGIFY_DIR/src/js/taggify-script.js $TAGGIFY_DIR/src/js/taggify.es6.js $TAGGIFY_DIR/src/js/taggify.min.js.gz $TAGGIFY_DIR/.bower.json $TAGGIFY_DIR/.gitignore $TAGGIFY_DIR/.travis.yml $TAGGIFY_DIR/db.json $TAGGIFY_DIR/gulpfile.js $TAGGIFY_DIR/index.html $TAGGIFY_DIR/karma.conf.js $TAGGIFY_DIR/module-generator.js $TAGGIFY_DIR/taggify-comment.js $TAGGIFY_DIR/template.common.js $TAGGIFY_DIR/template.es6.js
+rm -r "$TAGGIFY_DIR/test" "$TAGGIFY_DIR/src/css" $TAGGIFY_DIR/src/js/taggify-script.js $TAGGIFY_DIR/src/js/taggify.es6.js $TAGGIFY_DIR/src/js/taggify.min.js.gz $TAGGIFY_DIR/.travis.yml $TAGGIFY_DIR/db.json $TAGGIFY_DIR/gulpfile.js $TAGGIFY_DIR/index.html $TAGGIFY_DIR/karma.conf.js $TAGGIFY_DIR/module-generator.js $TAGGIFY_DIR/taggify-comment.js $TAGGIFY_DIR/template.common.js $TAGGIFY_DIR/template.es6.js
 check_process "clean Taggify"
 echo "This is a customized Taggify version." > $TAGGIFY_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $TAGGIFY_NOTICE
 
-echo "# Removing unused files from create-react-class"
-rm -rf "$CREATE_REACT_CLASS_DIR/node_modules" $CREATE_REACT_CLASS_DIR/create-react-class.js $CREATE_REACT_CLASS_DIR/factory.js $CREATE_REACT_CLASS_DIR/index.js $CREATE_REACT_CLASS_DIR/README.md
-check_process "clean create-react-class"
-echo "This is a customized create-react-class version." > $CREATE_REACT_CLASS_NOTICE
-echo "To decrease the size of the bundle, it includes production-only files" >> $CREATE_REACT_CLASS_NOTICE
-
-echo "# Removing unused files from prop-types"
-rm -rf "$PROP_TYPES_DIR/lib" "$PROP_TYPES_DIR/node_modules" $PROP_TYPES_DIR/CHANGELOG.md $PROP_TYPES_DIR/checkPropTypes.js $PROP_TYPES_DIR/factory.js $PROP_TYPES_DIR/factoryWithThrowingShims.js $PROP_TYPES_DIR/factoryWithTypeCheckers.js $PROP_TYPES_DIR/index.js $PROP_TYPES_DIR/prop-types.js $PROP_TYPES_DIR/README.md
-check_process "clean prop-types"
-echo "This is a customized prop-types version." > $PROP_TYPES_NOTICE
-echo "To decrease the size of the bundle, it includes production-only files" >> $PROP_TYPES_NOTICE
-
 echo "# Removing unused files from moment"
-rm -rf "$MOMENT_DIR/src"
+rm -r "$MOMENT_DIR/src"
 check_process "clean moment"
 echo "This is a customized moment version." > $MOMENT_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $MOMENT_NOTICE
 
 echo "# Removing unused files from moment-timezone"
-rm -rf "$MOMENT_TIMEZONE_DIR/data"
+rm -r "$MOMENT_TIMEZONE_DIR/data"
 check_process "clean moment-timezone"
 echo "This is a customized moment version." > $MOMENT_TIMEZONE_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $MOMENT_TIMEZONE_NOTICE
 
 echo "# Removing unused files from d3"
-rm -rf "$D3_DIR/node_modules" $D3_DIR/API.md $D3_DIR/CHANGES.md $D3_DIR/index.js $D3_DIR/ISSUE_TEMPLATE.md $D3_DIR/rollup.config.js $D3_DIR/rollup.node.js $D3_DIR/dist/d3.node.js $D3_DIR/dist/package.js
+rm -r "$D3_DIR/node_modules" $D3_DIR/CHANGES.md $D3_DIR/index.js $D3_DIR/dist/d3.node.js $D3_DIR/dist/package.js
 check_process "clean d3"
 echo "This is a customized d3 version." > $D3_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $D3_NOTICE
 
 echo "# Removing unused files from dagre-d3"
-rm -rf "$DAGRE_D3_DIR/node_modules" "$DAGRE_D3_DIR/lib" "$DAGRE_D3_DIR/dist/demo" $DAGRE_D3_DIR/.jscsrc $DAGRE_D3_DIR/.jshintrc $DAGRE_D3_DIR/bower.json $DAGRE_D3_DIR/index.js $DAGRE_D3_DIR/karma.conf.js $DAGRE_D3_DIR/karma.core.conf.js $DAGRE_D3_DIR/dist/dagre-d3.core.js $DAGRE_D3_DIR/dist/dagre-d3.core.min.js $DAGRE_D3_DIR/dist/dagre-d3.core.min.js.map
+rm -r "$DAGRE_D3_DIR/lib" "$DAGRE_D3_DIR/dist/demo" $DAGRE_D3_DIR/.jshintrc $DAGRE_D3_DIR/bower.json $DAGRE_D3_DIR/index.js $DAGRE_D3_DIR/karma.conf.js $DAGRE_D3_DIR/karma.core.conf.js $DAGRE_D3_DIR/dist/dagre-d3.core.js $DAGRE_D3_DIR/dist/dagre-d3.core.min.js $DAGRE_D3_DIR/dist/dagre-d3.core.min.js.map
 check_process "clean dagre-d3"
 echo "This is a customized dagre-d3 version." > $DAGRE_D3_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $DAGRE_D3_NOTICE
 
 echo "# Removing unused files from js-md5"
-rm -rf "$JS_MD5_DIR/src"
+rm -r "$JS_MD5_DIR/src"
 check_process "clean js-md5"
 echo "This is a customized js-md5 version." > $JS_MD5_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $JS_MD5_NOTICE
 
-echo "# Removing unused files from chart-js"
-rm -rf ""
-check_process "clean chart-js"
 echo "This is a customized chart-js version." > $CHART_JS_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $CHART_JS_NOTICE
 
-echo "# Removing unused files from chartjs-plugin-datalabels"
-rm -rf ""
-check_process "clean chartjs-plugin-datalabels"
 echo "This is a customized chartjs-plugin-datalabels version." > $CHARTJS_PLUGIN_DATALABELS_NOTICE
 echo "To decrease the size of the bundle, it includes production-only files" >> $CHARTJS_PLUGIN_DATALABELS_NOTICE
-
 
 echo "# Creating the custom branch: $TMP_BRANCH"
 git checkout -q -b "$TMP_BRANCH" > /dev/null
--- src/bundle/Resources/encore/ibexa.config.setup.js
+++ src/bundle/Resources/encore/ibexa.config.setup.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = (Encore) => {
     Encore.addAliases({
-        '@ibexa-admin-ui-assets': path.resolve(
-            './vendor/ibexa/admin-ui-assets'
-        ),
+        '@ibexa-admin-ui-assets': path.resolve('./vendor/ibexa/admin-ui-assets'),
+        '@ids-assets': path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/ids-assets/dist'),
+        '@ids-components': path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/ids-components/dist'),
     });
 };
