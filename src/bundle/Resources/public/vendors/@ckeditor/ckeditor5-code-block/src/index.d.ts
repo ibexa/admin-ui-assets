9342f73ebe91b76929ba0339886145f5a2ea8b7b
Dariusz Szut <dew326@gmail.com>
2026-03-03 12:06:20 +0100

    Version 4.6.28

---
+++ src/bundle/Resources/public/vendors/@ckeditor/ckeditor5-code-block/src/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+/**
+ * @module code-block
+ */
+export { default as CodeBlock } from './codeblock';
+export { default as CodeBlockEditing } from './codeblockediting';
+export { default as CodeBlockUI } from './codeblockui';
+export type { default as CodeBlockCommand } from './codeblockcommand';
+export type { default as IndentCodeBlockCommand } from './indentcodeblockcommand';
+export type { default as OutdentCodeBlockCommand } from './outdentcodeblockcommand';
+export type { CodeBlockConfig } from './codeblockconfig';
+import './augmentation';
