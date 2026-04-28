7118213ef6286503c6474991b59127f9dfe95236
Michał Grabowski <michal.grabowski@ibexa.co>
2026-04-28 14:34:18 +0200

    Version 5.0

--- src/bundle/Resources/public/vendors/react-dom/cjs/react-dom-server.edge.production.js
+++ src/bundle/Resources/public/vendors/react-dom/cjs/react-dom-server.edge.production.js
@@ -7251,11 +7251,11 @@ function getPostponedState(request) {
 }
 function ensureCorrectIsomorphicReactVersion() {
   var isomorphicReactPackageVersion = React.version;
-  if ("19.2.4" !== isomorphicReactPackageVersion)
+  if ("19.2.5" !== isomorphicReactPackageVersion)
     throw Error(
       'Incompatible React versions: The "react" and "react-dom" packages must have the exact same version. Instead got:\n  - react:      ' +
         (isomorphicReactPackageVersion +
-          "\n  - react-dom:  19.2.4\nLearn more: https://react.dev/warnings/version-mismatch")
+          "\n  - react-dom:  19.2.5\nLearn more: https://react.dev/warnings/version-mismatch")
     );
 }
 ensureCorrectIsomorphicReactVersion();
@@ -7509,4 +7509,4 @@ exports.resumeAndPrerender = function (children, postponedState, options) {
     startWork(request);
   });
 };
-exports.version = "19.2.4";
+exports.version = "19.2.5";
