84f27165e0fb8125849dc566629089f99685c455
tischsoic <symfiz@gmail.com>
2026-05-19 15:44:39 +0200

    Move IDS translation domain config to YAML

--- src/bundle/DependencyInjection/IbexaAdminUiAssetsExtension.php
+++ src/bundle/DependencyInjection/IbexaAdminUiAssetsExtension.php
@@ -8,9 +8,11 @@ declare(strict_types=1);
 
 namespace Ibexa\Bundle\AdminUiAssets\DependencyInjection;
 
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Yaml\Yaml;
 
 final class IbexaAdminUiAssetsExtension extends Extension implements PrependExtensionInterface
 {
@@ -20,10 +22,14 @@ final class IbexaAdminUiAssetsExtension extends Extension implements PrependExte
 
     public function prepend(ContainerBuilder $container): void
     {
-        $container->prependExtensionConfig('bazinga_js_translation', [
-            'active_domains' => [
-                'ibexa_ds_components',
-            ],
-        ]);
+        $this->prependBazingaJsTranslationConfiguration($container);
+    }
+
+    private function prependBazingaJsTranslationConfiguration(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/bazinga_js_translation.yaml';
+        $config = Yaml::parseFile($configFile);
+        $container->prependExtensionConfig('bazinga_js_translation', $config);
+        $container->addResource(new FileResource($configFile));
     }
 }
