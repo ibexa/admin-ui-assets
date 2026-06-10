ebe988d5ecb84ebc04a0851b6336330970ae17fb
Jakub Brzegowski <symfiz@gmail.com>
2026-06-10 10:33:18 +0200

    Add IDS components JS translation support (#49)
    Co-authored-by: Konrad Oboza <konrad.oboza@ibexa.co>

---
+++ src/bundle/DependencyInjection/IbexaAdminUiAssetsExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\AdminUiAssets\DependencyInjection;
+
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Yaml\Yaml;
+
+final class IbexaAdminUiAssetsExtension extends Extension implements PrependExtensionInterface
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        // Intentionally left empty: this bundle only prepends Bazinga JS translation config.
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependBazingaJsTranslationConfiguration($container);
+    }
+
+    private function prependBazingaJsTranslationConfiguration(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/bazinga_js_translation.yaml';
+        $config = Yaml::parseFile($configFile);
+        $container->prependExtensionConfig('bazinga_js_translation', $config);
+        $container->addResource(new FileResource($configFile));
+    }
+}
